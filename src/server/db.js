d3669f81e03f0a323e96fad3c9de77a7d820636c
Dittonal <53260392+Dittonal@users.noreply.github.com>
2022-12-28 19:47:16 +0800

    4 files modified

--- src/server/db.js
+++ src/server/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   mysql: {
-    host: 'localhost', // 新建数据库连接时的 主机名或ID地址 内容
+    host: 'db4free.net', // 新建数据库连接时的 主机名或ID地址 内容
     user: 'gityu2e', // 连接的数据用户名
     password: '12345678', // 密码
     database: 'teat12', // 数据库名
